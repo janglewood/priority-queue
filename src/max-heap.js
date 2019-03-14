7c7d91a41ebce949a8f8b7c6ce9fdc1ef336c3cc
janglewood <zhl96@mail.ru>
2019-03-14 23:28:50 +0300

    feat: add initial methods

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,15 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];	
 	}
 
 	push(data, priority) {
-		
+		let node1 = new Node(data, priority);
+		let node2 = new Node(data, priority);
+		insertNode(node1);
+		shiftNodeUp(node2);	
 	}
 
 	pop() {
@@ -34,7 +38,17 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		if(!this.root || this.root.priority < node.priority) {
+			node.next = this.root;
+			this.root = node;
+		} else {
+			while(this.root.next && node.priority < this.root.next.priority) {
+				this.root = this.root.next;
+			}
+			node.next = this.root.next;
+			this.root.next = node;
+		}
+		this.parentNodes.push(node);	
 	}
 
 	shiftNodeUp(node) {
