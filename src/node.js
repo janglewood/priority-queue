0a070904e365aee6278a9cd4783e236aaf4295f6
Anton Karatkevich <zhl96@mail.ru>
2019-03-24 00:06:06 +0300

    fix: implement node class

--- src/node.js
+++ src/node.js
@@ -30,7 +30,64 @@ class Node {
 
 	swapWithParent() {
 		if(!this.parent) return;
-		this.parent.parent = this;
+
+		let childIsLeft;
+		let parentIsLeft;
+		let leftChild;
+    	let rightChild;
+    	let root;
+
+		if(this.parent && this.parent.left === this) {
+			childIsLeft = true;
+		} else if (this.parent && this.parent.right === this) {
+			childIsLeft = false;
+		}
+		if(this.parent.parent && this.parent.parent.left === this.parent) {
+      		parentIsLeft = true;
+      		root = this.parent.parent;
+		} else if(this.parent.parent && this.parent.parent.right === this.parent) {
+      		parentIsLeft = false;
+      		root = this.parent.parent;
+		}
+		this.left ? leftChild = this.left : null;
+		this.right ? rightChild = this.right : null;
+
+		if(childIsLeft) {
+			if(leftChild || rightChild) {
+				leftChild ? this.parent.left = leftChild : null;
+				rightChild ? this.parent.right = rightChild : null;
+			}
+			this.parent.right ? this.right = this.parent.right : null;
+
+			if(parentIsLeft) {
+				this.parent.parent.left = this;
+			} else if(this.parent.parent && !parentIsLeft) {
+				this.parent.parent.right = this;
+			}
+		
+      		this.left = this.parent;
+	  		this.left.parent = this;
+	  		this.right ? this.right.parent = this : null;
+			this.parent = root;
+			  
+		} else {
+			if(leftChild || rightChild) {
+				leftChild ? this.parent.left = leftChild : null;
+				rightChild ? this.parent.right = rightChild : null;
+			}
+			this.parent.left ? 	this.left = this.parent.left : null;
+
+			if(this.parent.parent && parentIsLeft) {
+				this.parent.parent.left = this;
+			} else if(this.parent.parent && !parentIsLeft) {
+				this.parent.parent.right = this;
+			}
+
+      		this.right = this.parent;
+	  		this.right.parent = this;
+	 		this.left ? this.left.parent = this : null;
+     		this.parent = root;
+		}
 	}
 }
 
